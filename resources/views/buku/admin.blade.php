2b6b48b8d3d0619c4b0244f977f9bd3b601eef2a
MelindaSariSP <143081915+MelindaSariSP@users.noreply.github.com>
2025-05-20 12:35:12 +0700

    Dashboard admin

---
+++ resources/views/buku/admin.blade.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="frame">
+      <div class="div">
+        <div class="content">
+          <div class="overlap">
+            <div class="text-wrapper">Pengunjung Fakultas Terbanyak</div>
+            <div class="bar-line-chart">
+              <div class="chart-axis">
+                <div class="main-chart">
+                  <div class="y-axis-left">
+                    <div class="text-wrapper-2">100</div>
+                    <div class="text-wrapper-3">80</div>
+                    <div class="text-wrapper-3">60</div>
+                    <div class="text-wrapper-3">40</div>
+                    <div class="text-wrapper-3">20</div>
+                    <div class="text-wrapper-3">0</div>
+                  </div>
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group">
+                      <div class="x-lines">
+                        <img class="line" src="img/line-7.svg" />
+                        <img class="img" src="img/line-12.svg" />
+                        <img class="img" src="img/line-21.svg" />
+                        <img class="img" src="img/line-3.svg" />
+                        <img class="img" src="img/line-11.svg" />
+                        <img class="img" src="img/line-13.svg" />
+                      </div>
+                      <div class="y-lines">
+                        <img class="line-2" src="img/line-4.svg" />
+                        <img class="line-3" src="img/line-22.svg" />
+                        <img class="line-3" src="img/line.svg" />
+                        <img class="line-3" src="img/line-19.svg" />
+                        <img class="line-3" src="img/line-20.svg" />
+                        <img class="line-3" src="img/line-2.svg" />
+                      </div>
+                      <div class="bar-area">
+                        <div class="bar-group">
+                          <div class="overlap-group-wrapper">
+                            <div class="overlap-group-2">
+                              <div class="bar-BG"></div>
+                              <div class="single-bar"></div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="bar-group">
+                          <div class="overlap-group-wrapper">
+                            <div class="overlap-group-2">
+                              <div class="bar-BG"></div>
+                              <div class="single-bar-2"></div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="bar-group">
+                          <div class="overlap-group-wrapper">
+                            <div class="overlap-group-2">
+                              <div class="bar-BG"></div>
+                              <div class="single-bar-3"></div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="bar-group">
+                          <div class="overlap-group-wrapper">
+                            <div class="overlap-group-2">
+                              <div class="bar-BG"></div>
+                              <div class="single-bar-3"></div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="bar-group">
+                          <div class="overlap-group-wrapper">
+                            <div class="overlap-group-2">
+                              <div class="bar-BG"></div>
+                              <div class="bar-strip-wrapper"><div class="bar-strip"></div></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="x-axis">
+                  <div class="x-label-box"><div class="text-wrapper-4">Januari</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">Febuari</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">Maret</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">April</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">Mei</div></div>
+                </div>
+              </div>
+              <div class="legends">
+                <div class="legend-wrapper">
+                  <div class="legend">
+                    <div class="legend-node">
+                      <div class="basic-node"><div class="square-fill"></div></div>
+                    </div>
+                    <div class="text-wrapper-5">2025</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="overlap-2">
+            <div class="bar-line-chart-2">
+              <div class="chart-axis-2">
+                <div class="main-chart">
+                  <div class="y-axis-left">
+                    <div class="text-wrapper-2">100</div>
+                    <div class="text-wrapper-3">80</div>
+                    <div class="text-wrapper-3">60</div>
+                    <div class="text-wrapper-3">40</div>
+                    <div class="text-wrapper-3">20</div>
+                    <div class="text-wrapper-3">0</div>
+                  </div>
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-3">
+                      <div class="x-lines-2">
+                        <img class="line" src="img/line-6.svg" />
+                        <img class="img" src="img/line-15.svg" />
+                        <img class="img" src="img/line-17.svg" />
+                        <img class="img" src="img/line-16.svg" />
+                        <img class="img" src="img/line-5.svg" />
+                        <img class="img" src="img/line-9.svg" />
+                      </div>
+                      <div class="y-lines-2">
+                        <img class="line-2" src="img/line-14.svg" />
+                        <img class="line-3" src="img/image.svg" />
+                        <img class="line-3" src="img/line-10.svg" />
+                        <img class="line-3" src="img/line-18.svg" />
+                        <img class="line-3" src="img/line-23.svg" />
+                        <img class="line-3" src="img/line-8.svg" />
+                      </div>
+                      <div class="line-area">
+                        <div class="line-group">
+                          <div class="line-nodes-wrapper">
+                            <div class="line-nodes">
+                              <div class="ellipse-line-wrapper"><div class="ellipse-line"></div></div>
+                              <div class="div-wrapper"><div class="ellipse-line"></div></div>
+                              <div class="basic-node-2"><div class="ellipse-line"></div></div>
+                              <div class="basic-node-3"><div class="ellipse-line"></div></div>
+                              <div class="basic-node-4"><div class="ellipse-line"></div></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="x-axis">
+                  <div class="x-label-box"><div class="text-wrapper-4">Januari</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">Febuari</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">Maret</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">April</div></div>
+                  <div class="x-label-box"><div class="text-wrapper-4">Mei</div></div>
+                </div>
+              </div>
+              <div class="line-legends-wrapper">
+                <div class="legend-wrapper">
+                  <div class="legend">
+                    <div class="legend-node">
+                      <div class="basic-node-wrapper">
+                        <div class="basic-node-5"><div class="ellipse-line"></div></div>
+                      </div>
+                    </div>
+                    <div class="text-wrapper-5">2025</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="text-wrapper-6">Peminjam Buku Fakultas Terbanyak</div>
+          </div>
+          <div class="overlap-4">
+            <div class="rectangle"></div>
+            <div class="text-wrapper-7">150</div>
+            <div class="rectangle-2"></div>
+            <div class="rectangle-3"></div>
+            <div class="rectangle-4"></div>
+            <div class="text-wrapper-8">80</div>
+            <div class="text-wrapper-9">Pend. Biologi</div>
+            <div class="text-wrapper-10">Total Pengunjung</div>
+            <div class="text-wrapper-11">Pengunjung Fakultas Terbanyak</div>
+          </div>
+          <div class="overlap-5">
+            <div class="overlap-6">
+              <div class="text-wrapper-12">70</div>
+              <div class="text-wrapper-13">Total Peminjam Buku</div>
+            </div>
+            <div class="rectangle-5"></div>
+          </div>
+          <div class="overlap-7">
+            <div class="overlap-8">
+              <div class="text-wrapper-14">Teknik Informatika</div>
+              <div class="text-wrapper-15">Peminjam Buku Fakultas Terbanyak</div>
+              <div class="text-wrapper-16">67</div>
+            </div>
+            <div class="rectangle-6"></div>
+          </div>
+        </div>
+        <div class="side-bar">
+          <div class="overlap-9">
+            <div class="rectangle-7"></div>
+            <div class="text-wrapper-17">Pengaturan akun</div>
+            <div class="group"></div>
+            <img class="image" src="img/image-5.png" />
+            <div class="group-2">
+              <div class="overlap-group-3">
+                <div class="text-wrapper-18">Dashboard</div>
+                <img class="vector" src="img/vector-3.svg" />
+              </div>
+            </div>
+            <div class="group-3">
+              <div class="text-wrapper-19">Data Pengunjung</div>
+              <img class="people" src="img/people.png" />
+            </div>
+            <div class="group-4">
+              <div class="text-wrapper-20">Data Peminjaman Buku</div>
+              <img class="vector-2" src="img/vector.svg" />
+            </div>
+            <div class="group-5">
+              <div class="text-wrapper-21">Data Akun</div>
+              <img class="vector-3" src="img/vector-2.svg" />
+            </div>
+            <div class="group-6">
+              <div class="text-wrapper-21">Jumlah Koleksi</div>
+              <div class="icon">
+                <div class="group-7">
+                  <div class="overlap-group-4">
+                    <img class="group-8" src="img/group-3.png" />
+                    <img class="group-9" src="img/image.png" />
+                    <img class="group-10" src="img/group-2.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <header class="header">
+          <div class="overlap-10">
+            <div class="overlap-group-5">
+              <img class="selamat-datang-admin" src="img/selamat-datang-admin.png" />
+              <p class="home-dashboard">
+                <span class="span">Home &gt; </span> <span class="text-wrapper-22">Dashboard </span>
+              </p>
+            </div>
+            <div class="overlap-11">
+              <img class="search" src="img/search.png" />
+              <div class="text-wrapper-23">Cari......</div>
+            </div>
+            <img class="vector-4" src="img/vector-4.svg" />
+            <div class="text-wrapper-24">Admin</div>
+          </div>
+        </header>
+      </div>
+    </div>
+  </body>
+</html>
