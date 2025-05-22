91a3e7ff3af2a815f5800b0e0d5568909144c27f
MelindaSariSP <143081915+MelindaSariSP@users.noreply.github.com>
2025-05-22 22:00:42 +0700

    admin_dataakun

---
+++ resources/views/admin_dataakun.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="admin-dataakun">
+      <div class="div">
+        <div class="content">
+          <div class="overlap">
+            <div class="text-wrapper">2025-03-04</div>
+            <div class="text-wrapper-2">2025-02-20</div>
+            <div class="text-wrapper-3">2025-03-10</div>
+            <div class="text-wrapper-4">2025-02-05</div>
+            <div class="text-wrapper-5">2025-03-12</div>
+            <div class="text-wrapper-6">2025-03-17</div>
+            <div class="rectangle"></div>
+            <div class="frame">
+              <div class="frame-2">
+                <div class="div-wrapper"><div class="text-wrapper-7">No</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-8">Nama</div></div>
+                <div class="frame-3"><div class="text-wrapper-9">Username</div></div>
+                <div class="frame-3"><div class="text-wrapper-10">Email</div></div>
+                <div class="frame-3"><div class="text-wrapper-11">Status Akun</div></div>
+                <div class="frame-3"><div class="text-wrapper-12">Tanggal dibuat</div></div>
+                <div class="frame-3"><div class="text-wrapper-13">Aksi</div></div>
+              </div>
+              <div class="frame-4">
+                <div class="div-wrapper">
+                  <div class="text-wrapper-14">1</div>
+                </div>
+                <div class="frame-3">
+                  <a
+                    href="https://chatgpt.com/c/681a0bb2-5438-8005-9b9e-1c1d919c7a36#:~:text=1-,Rian%20Maulana,-rianm"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div class="text-wrapper-15">Rian Maulana</div>
+                  </a>
+                </div>
+                <div class="frame-3"><div class="text-wrapper-16">rianm</div></div>
+                <div class="frame-3"><div class="text-wrapper-17">rian@email.com</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Aktif</div></div>
+                <div class="frame-5"></div>
+                <div class="div-wrapper">
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group"><img class="edit" src="img/edit.png" /></div>
+                  </div>
+                </div>
+              </div>
+              <div class="frame-4">
+                <div class="div-wrapper"><div class="text-wrapper-7">2</div></div>
+                <div class="frame-3"><div class="text-wrapper-18">Dinda Ayu Pratiwi</div></div>
+                <div class="frame-3"><div class="text-wrapper-19">dindaa</div></div>
+                <div class="frame-3"><div class="text-wrapper-20">dinda.pratiwi@email.com</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Non Aktif</div></div>
+                <div class="frame-5"></div>
+                <div class="div-wrapper">
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group"><img class="edit" src="img/edit-3.png" /></div>
+                  </div>
+                </div>
+              </div>
+              <div class="frame-4">
+                <div class="div-wrapper"><div class="text-wrapper-7">3</div></div>
+                <div class="frame-3"><div class="ramadhan-rama">Ramadhan&nbsp;&nbsp;Rama</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">ramaa</div></div>
+                <div class="frame-3"><div class="text-wrapper-21">rama_dhan@email.com</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Aktif</div></div>
+                <div class="frame-5"></div>
+                <div class="div-wrapper">
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group"><img class="edit" src="img/edit-2.png" /></div>
+                  </div>
+                </div>
+              </div>
+              <div class="frame-4">
+                <div class="div-wrapper"><div class="text-wrapper-7">4</div></div>
+                <div class="frame-3"><div class="text-wrapper-22">Indah Lestari</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Lestari</div></div>
+                <div class="frame-3"><div class="text-wrapper-23">indh@email.com</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Aktif</div></div>
+                <div class="frame-5"></div><div class="div-wrapper">
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group"><img class="edit" src="img/edit-4.png" /></div>
+                  </div>
+                </div>
+              </div>
+             <div class="frame-4">
+                <div class="div-wrapper"><div class="text-wrapper-7">5</div></div>
+                <div class="frame-3"><div class="text-wrapper-24">Nur Hidayat</div></div>
+                <div class="frame-3"><div class="text-wrapper-25">Nurdayat</div></div>
+                <div class="frame-3"><div class="text-wrapper-26">dayat@email.com</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Non Aktif</div></div>
+                <div class="frame-5"></div><div class="div-wrapper">
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group"><img class="edit" src="img/edit-6.png" /></div>
+                 </div>
+              </div>
+            </div>
+            <div class="frame-4">
+                <div class="div-wrapper"><div class="text-wrapper-7">6</div></div>
+                <div class="frame-3"><div class="text-wrapper-27">Maya Damayanti</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Maydti</div></div>
+                <div class="frame-3"><div class="text-wrapper-28">damynti_maya@email.com</div></div>
+                <div class="div-wrapper"><div class="text-wrapper-7">Aktif</div></div>
+                <div class="frame-5"></div><div class="div-wrapper">
+                  <div class="overlap-group-wrapper">
+                    <div class="overlap-group"><img class="edit" src="img/edit-5.png" /></div>
+               </div>
+              </div>
+            </div>
+            <img class="table" src="img/table.png" />
+            <div class="text-wrapper-29">Tabel Data Pengunjung</div>
+            <div class="rectangle-2"></div>
+            <div class="rectangle-3"></div>
+            <div class="rectangle-4"></div>
+            <div class="rectangle-5"></div>
+            <div class="rectangle-6"></div>
+            <div class="text-wrapper-30">Cari</div>
+            <img class="search" src="img/search.png" />
+            <div class="text-wrapper-31">2025</div>
+            <img class="sort-down" src="img/sort-down.png" />
+            <img class="document" src="img/document.png" />
+            <img class="refresh" src="img/refresh.png" />
+            <img class="delete" src="img/delete.png" />
+            </div>
+            <div class="overlap-2">
+            <div class="rectangle-7"></div>
+            <div class="text-wrapper-32">150</div>
+            <div class="rectangle-8"></div>
+            <div class="rectangle-9"></div>
+            <div class="rectangle-10"></div>
+            <div class="text-wrapper-33">80</div>
+            <img class="chevron-down" src="img/image.png" />
+            <div class="text-wrapper-34">TEKNIK</div>
+            <div class="text-wrapper-35">Total Pengunjung</div>
+            <div class="pengunjung-terbanyak">Pengunjung&nbsp;&nbsp;Terbanyak</div>
+            <div class="text-wrapper-36">Fakultas</div>
+            </div>
+            <div class="overlap-3">
+            <div class="overlap-4">
+                <div class="text-wrapper-37">70</div>
+                <div class="text-wrapper-38">Total Peminjam Buku</div>
+            </div>
+            <div class="rectangle-11"></div>
+            </div>
+
+            <div class="overlap-5">
+            <div class="overlap-6">
+                <img class="img" src="img/chevron-down.png" />
+                <div class="text-wrapper-39">FMIPA</div>
+                <div class="text-wrapper-40">Fakultas</div>
+                <div class="overlap-7">
+                <div class="text-wrapper-41">Peminjam Buku Terbanyak</div>
+                <div class="text-wrapper-42">67</div>
+                </div>
+            </div>
+            <div class="rectangle-12"></div>
+            </div>
+            <div class="side-bar">
+            <div class="overlap-8">
+                <div class="rectangle-13"></div>
+                <div class="text-wrapper-43">Pengaturan akun</div>
+                <div class="rectangle-14"></div>
+                <div class="group"></div>
+                <img class="image" src="img/image-6.png" />
+                <div class="text-wrapper-44">Dashboard</div>
+                <img class="vector" src="img/image.svg" />
+                <div class="group-2">
+                <div class="text-wrapper-45">Data Pengunjung</div>
+                <img class="people" src="img/people.png" />
+                </div>
+                <div class="group-3">
+                    <div class="text-wrapper-46">Data Peminjaman Buku</div>
+                    <img class="vector-2" src="img/vector-3.svg" />
+                </div>
+                <div class="group-4">
+                    <div class="text-wrapper-47">Data Akun</div>
+                    <img class="vector-3" src="img/vector.svg" />
+                </div>
+                <div class="group-5">
+                    <div class="text-wrapper-47">Jumlah Koleksi</div>
+                    <div class="icon">
+                     <div class="group-6">
+                      <div class="overlap-group-2">
+                        <img class="group-7" src="img/group-4.png" />
+                        <img class="group-8" src="img/group.png" />
+                        <img class="group-9" src="img/group-3.png" />
+                      </div>
+                     </div>
+                    </div>
+                   </div>
+                  </div>
+                 </div>
+                <header class="header">
+                <div class="overlap-9">
+                    <img class="vector-4" src="img/vector-2.svg" />
+                    <div class="text-wrapper-48">Admin</div>
+                    <div class="overlap-group-3">
+                    <p class="home-dashboard-data">
+                        <span class="span">Home &gt; Dashboard &gt; </span>
+                        <span class="text-wrapper-49">Data Akun</span>
+                    </p>
+                    <div class="text-wrapper-50">Selamat Datang, Admin</div>
+                    </div>
+                   </div>
+                  </header>
+                 </div>
+                </div>
+               </body>
+              </html>
+
+
