f8aedfa71fb8c26b569e8621ee888d35b2dc04c5
MelindaSariSP <143081915+MelindaSariSP@users.noreply.github.com>
2025-05-23 00:17:37 +0700

    admin_jumlahkoleksi1

---
+++ resources/views/admin/admin_jumlahkoleksi1.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="admin">
+      <div class="admin-jumlahkoleksi">
+        <div class="content">
+          <div class="overlap-group">
+            <div class="text-wrapper">Infomasi Jumlah Koleksi</div>
+            <div class="overlap">
+              <div class="rectangle"></div>
+              <div class="div">Jumlah</div>
+              <div class="text-wrapper-2">Jenis Koleksi</div>
+              <div class="rectangle-2"></div>
+              <div class="rectangle-3"></div>
+              <div class="rectangle-4"></div>
+              <div class="text-wrapper-3">Buku</div>
+              <div class="text-wrapper-4">1120</div>
+              <div class="text-wrapper-5">:</div>
+            </div>
+            <div class="overlap-2">
+              <div class="text-wrapper-6">Jurnal</div>
+              <div class="div-wrapper">
+                <div class="text-wrapper-7">:</div>
+              </div>
+              <div class="text-wrapper-8">1115</div>
+            </div>
+            <div class="overlap-3">
+              <div class="rectangle-5"></div>
+              <div class="text-wrapper-9">Karya Ilmiah</div>
+              <div class="rectangle-6"></div>
+              <div class="text-wrapper-10">1110</div>
+              <div class="text-wrapper-11">:</div>
+            </div>
+            <div class="overlap-4">
+              <div class="overlap-5">
+                <div class="text-wrapper-12">:</div>
+              </div>
+              <div class="text-wrapper-13">Total Koleksi</div>
+              <div class="text-wrapper-14">3345</div>
+            </div>
+            <div class="overlap-6">
+                <img class="group" src="img/group-3.png" />
+                <img class="img" src="img/image.png" />
+                <img class="group-2" src="img/group-5.png" />
+             </div>
+           </div>
+            <div class="overlap-7">
+                <div class="rectangle-7"></div>
+                <div class="text-wrapper-15">150</div>
+                <div class="rectangle-8"></div>
+                <div class="rectangle-9"></div>
+                <div class="rectangle-10"></div>
+                <div class="text-wrapper-16">80</div>
+                <img class="chevron-down" src="img/chevron-down.png" />
+                <div class="text-wrapper-17">TEKNIK</div>
+                <div class="text-wrapper-18">Total Pengunjung</div>
+                <div class="pengunjung-terbanyak">Pengunjung&nbsp;&nbsp;Terbanyak</div>
+                <div class="text-wrapper-19">Fakultas</div>
+              </div>
+            <div class="overlap-8">
+            <div class="overlap-9">
+                <div class="text-wrapper-20">70</div>
+                <div class="text-wrapper-21">Total Peminjam Buku</div>
+            </div>
+            <div class="rectangle-11"></div>
+            </div>
+             <div class="overlap-10">
+              <div class="overlap-11">
+                <img class="chevron-down-2" src="img/chevron-down-2.png" />
+                <div class="text-wrapper-22">FMIPA</div>
+                <div class="text-wrapper-23">Fakultas</div>
+                <div class="overlap-12">
+                 <div class="text-wrapper-24">Peminjam Buku Terbanyak</div>
+                  <div class="text-wrapper-25">67</div>
+                </div>
+              </div>
+            <div class="rectangle-12"></div>
+            </div>
+           </div>
+           <div class="side-bar">
+            <div class="overlap-13">
+                <div class="rectangle-13"></div>
+                <div class="text-wrapper-26">Pengaturan akun</div>
+                <img class="rectangle-14" src="img/rectangle-15.png" />
+                <div class="group-3"></div>
+                <img class="image" src="img/image-6.png" />
+                <div class="text-wrapper-27">Dashboard</div>
+                <img class="vector" src="img/image.svg" />
+                
+                <div class="group-4">
+                <div class="text-wrapper-28">Data Pengunjung</div>
+                <img class="people" src="img/people.png" />
+                </div>
+
+                <div class="group-5">
+                <div class="text-wrapper-29">Data Peminjaman Buku</div>
+                <img class="vector-2" src="img/vector-3.svg" />
+                </div>
+
+                <div class="group-6">
+                <div class="text-wrapper-30">Data Akun</div>
+                <img class="vector-3" src="img/vector.svg" />
+                </div>
+
+                <div class="group-7">
+                <div class="text-wrapper-30">Jumlah Koleksi</div>
+                <div class="icon">
+                    <div class="overlap-group-wrapper">
+                    <div class="overlap-group-2">
+                        <img class="group-8" src="img/group-2.png" />
+                        <img class="group-9" src="img/group-4.png" />
+                        <img class="group-10" src="img/group.png" />
+                       </div>
+                      </div>
+                     </div>
+                    </div>
+                  </div>
+                </div>
+                <header class="header">
+                    <div class="overlap-14">
+                        <img class="vector-4" src="img/vector-2.svg" />
+                        <div class="text-wrapper-31">Admin</div>
+                        <div class="overlap-group-3">
+                        <p class="home-dashboard-data">
+                            <span class="span">Home &gt; Dashboard &gt; </span>
+                            <span class="text-wrapper-32">Data Peminjaman Buku</span>
+                        </p>
+                        <img class="selamat-datang-admin" src="img/selamat-datang-admin.png" />
+                        </div>
+                       </div>
+                      </header>
+                     </div>
+                    </div>
+                   </body>
+                  </html>
+
+                
+
