4e5d4f4755cbb5d59c8ec1ed4617bcd3a410668d
MelindaSariSP <143081915+MelindaSariSP@users.noreply.github.com>
2025-05-23 21:59:06 +0700

    admin_jumlahkoleksi2

---
+++ resources/views/admin/admin_jumlahkoleksi2.blade.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Dashboard Admin</title>
+  </head>
+  <body>
+    <div class="admin">
+      <div class="admin-jumlahkoleksi">
+        <div class="content">
+          <div class="overlap-group">
+            <div class="overlap">
+              <div class="text-wrapper">Update</div>
+            </div>
+            <div class="div">Jumlah Buku</div>
+
+            <div class="overlap-2">
+              <div class="text-wrapper-2">Jumlah Jurnal</div>
+              <div class="rectangle"></div>
+            </div>
+
+            <div class="overlap-3">
+              <div class="jumlah-karya-ilmiah">Jumlah Karya&nbsp;&nbsp;Ilmiah</div>
+              <div class="rectangle-2"></div>
+            </div>
+
+            <div class="text-wrapper-3">Jumlah Total Koleksi</div>
+            <div class="rectangle-3"></div>
+            <div class="rectangle-4"></div>
+            <div class="text-wrapper-4">Edit Informasi Jumlah Koleksi</div>
+          </div>
+
+          <div class="overlap-4">
+            <div class="rectangle-5"></div>
+            <div class="text-wrapper-5">150</div>
+            <div class="rectangle-6"></div>
+            <div class="rectangle-7"></div>
+            <div class="rectangle-8"></div>
+            <div class="text-wrapper-6">80</div>
+            <img class="chevron-down" src="img/chevron-down.png" alt="Dropdown Icon" />
+            <div class="text-wrapper-7">TEKNIK</div>
+            <div class="text-wrapper-8">Total Pengunjung</div>
+            <div class="pengunjung-terbanyak">Pengunjung&nbsp;&nbsp;Terbanyak</div>
+            <div class="text-wrapper-9">Fakultas</div>
+          </div>
+          <div class="overlap-5">
+        <div class="overlap-6">
+            <div class="text-wrapper-10">70</div>
+            <div class="text-wrapper-11">Total Peminjam Buku</div>
+        </div>
+        <div class="rectangle-9"></div>
+        </div>
+        <div class="overlap-7">
+        <div class="overlap-8">
+            <img class="img" src="img/chevron-down-2.png" alt="Dropdown Icon" />
+            <div class="text-wrapper-12">FMIPA</div>
+            <div class="text-wrapper-13">Fakultas</div>
+            <div class="overlap-9">
+            <div class="text-wrapper-14">Peminjam Buku Terbanyak</div>
+            <div class="text-wrapper-15">67</div>
+            </div>
+        </div>
+        <div class="rectangle-10"></div>
+        </div>
+        <div class="side-bar">
+        <div class="overlap-10">
+            <div class="rectangle-11"></div>
+            <div class="text-wrapper-16">Pengaturan Akun</div>
+            <img class="rectangle-12" src="img/rectangle-15.png" alt="Foto Profil" />
+            <div class="group"></div>
+            <img class="image" src="img/image-6.png" alt="Icon Dashboard" />
+            <div class="text-wrapper-17">Dashboard</div>
+            <img class="vector" src="img/vector.svg" alt="Ikon" />
+
+            <div class="group-2">
+            <div class="text-wrapper-18">Data Pengunjung</div>
+            <img class="people" src="img/people.png" alt="Ikon Pengunjung" />
+            </div>
+
+            <div class="group-3">
+            <div class="text-wrapper-19">Data Peminjaman Buku</div>
+            <img class="vector-2" src="img/vector-2.svg" alt="Ikon Peminjaman" />
+            </div>
+
+            <div class="group-4">
+            <div class="text-wrapper-20">Data Akun</div>
+            <img class="vector-3" src="img/image.svg" alt="Ikon Akun" />
+            </div>
+            <!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Edit Jumlah Koleksi</title>
+  <link rel="stylesheet" href="globals.css" />
+  <link rel="stylesheet" href="styleguide.css" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div class="group-5">
+    <div class="text-wrapper-20">Jumlah Koleksi</div>
+    <div class="icon">
+      <div class="overlap-group-wrapper">
+        <div class="overlap-group-2">
+          <img class="group-6" src="img/group-3.png" alt="Icon Buku" />
+          <img class="group-7" src="img/group.png" alt="Icon Koleksi" />
+          <img class="group-8" src="img/image.png" alt="Icon Tambahan" />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div> 
+  <header class="header">
+    <div class="overlap-11">
+      <img class="vector-4" src="img/vector-3.svg" alt="Icon Admin" />
+      <div class="text-wrapper-21">Admin</div>
+      <div class="overlap-group-3">
+        <img class="selamat-datang-admin" src="img/selamat-datang-admin.png" alt="Selamat Datang Admin" />
+        <p class="home-dahboard-jumlah">
+          <span class="span">Home &gt; Dashboard &gt; Jumlah Koleksi &gt;</span>
+          <span class="text-wrapper-22">&nbsp;</span>
+          <span class="text-wrapper-23">Edit Informasi Jumlah Koleksi</span>
+        </p>
+      </div>
+    </div>
+  </header>
+</div>
+</div>
+</body>
+</html>
+
+        
+
+       
